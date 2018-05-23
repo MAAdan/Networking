4c3844f71c493aea171b1c75a719a5717660761e
MAAdan <adan.miguel@gmail.com>
2018-05-23 23:30:29 +0200

    Initial Commit

---
+++ Networking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Miguel Angel Adan Roman on 23/5/18.
+//  Copyright © 2018 Avantiic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
